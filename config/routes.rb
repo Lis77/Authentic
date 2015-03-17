391c7877726f785a82091277c1183ec543f9e312
Lis77 <melssadawnmartell@gmail.com>
2015-03-17 20:59:27 +0000

    Sign In and Sign Out working

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   
   
   root 'users#index'
+  
+  delete 'sign_out' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
